30f63447b0a454dc0cf9b73c9e419b3343310d9c
sirProgrammer <andrejzavalnickij0@gmail.com>
2023-03-03 18:49:29 +0100

    plan cards are in column on phones

--- src/components/PlanCard.jsx
+++ src/components/PlanCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import './styling/PlanCard.css'
+import './styling/PlanCard.css';
+
 
 const PlanCard = ({ image, title, price, ingredients}) => {
+
   return (
     <div className="planCard">
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li key={ingredient}>{ingredient}</li>
-        ))}
-      </ul>
-      <div className="planCardFooter">
-        <p>{price}</p>
-        
-      </div>
+          <img src={image} alt={title} />
+          <h3>{title}</h3>
+          <ul>
+            {ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <div className="planCardFooter">
+            <p>{price}</p>
+          </div>
     </div>
   );
 };
